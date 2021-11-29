a1ef0a007ab38755f3d27da4b94c48a35f4b517d
salvarezri <santiagoalvarezricardo@gmail.com>
2021-11-29 15:51:31 -0500

    Update MathFunctions.java

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -2,4 +2,7 @@ public class MathFunctions {
     public static int multiplyNumber(int numberA, int numberB) {
         return numberA * numberB;
     }
+    public static String concatenate(String strA, String strB){
+        return strA.concat(strB);
+    }
 }
