f93de443d256bb3daa7bc7a1223c3af08d97c921
Diego Fernando Bello Lopez <56003957+dfbello@users.noreply.github.com>
2021-11-29 21:11:11 -0500

    Agregada potencia de numeros enteros

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -5,4 +5,13 @@ public class MathFunctions {
     public static String concatenate(String strA, String strB){
         return strA.concat(strB);
     }
+    public static int potInt(int a, int b){
+	    if(b == 0){
+			return 1;
+		}
+		if(b == 1){
+			return a;
+		}
+		return a * potInt(a,b-1);
+	}
 }
